ef48efcd60b0aac7e9068a245de649d698488e1d
MojTabaa4 <mojtabablue12@gmail.com>
2023-06-10 11:11:15 +0330

    make some refinement in wall class

--- src/com/company/Wall.java
+++ src/com/company/Wall.java
@@ -4,12 +4,13 @@ import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
 public class Wall extends Line2D.Double {
-    private Point2D.Double pt1, pt2;
+    private final Point2D.Double pt1;
+    private final Point2D.Double pt2;
 
     public Wall(int x1, int y1, int x2, int y2) {
         pt1 = new Point2D.Double(x1, y1);
         pt2 = new Point2D.Double(x2, y2);
-        super.setLine(pt1, pt2);
+        setLine(pt1, pt2);
     }
 
     public Point2D.Double getPt1() {
@@ -21,6 +22,9 @@ public class Wall extends Line2D.Double {
     }
 
     public Point2D.Double getMidpoint() {
-        return new Point2D.Double((pt1.getX() + pt2.getX()) / 2, (pt1.getY() + pt2.getY()) / 2);
+        double midX = (pt1.getX() + pt2.getX()) / 2;
+        double midY = (pt1.getY() + pt2.getY()) / 2;
+
+        return new Point2D.Double(midX, midY);
     }
 }
